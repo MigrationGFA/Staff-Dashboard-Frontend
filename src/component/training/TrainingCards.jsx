20b16111ad1bee73a9096083d9d5ad7a7c994384
Awoleye-Bolaji <bolaji@gfa-tech.com>
2025-03-02 02:01:06 +0100

    add training page

---
+++ src/component/training/TrainingCards.jsx
@@ -0,0 +1,5 @@
+const TrainingCards = () => {
+  return <div>TrainingCards</div>;
+};
+
+export default TrainingCards;
--- src/pages/Training.jsx
+++ src/pages/Training.jsx
@@ -1,7 +1,13 @@
+import TrainingCards from "../component/training/TrainingCards";
+import TrainingHeader from "../component/training/TrainingHeader";
 import DashboardLayout from "../layout/DashboardLayout";
 
 const Training = () => {
-  return <DashboardLayout></DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <TrainingHeader /> <TrainingCards />
+    </DashboardLayout>
+  );
 };
 
 export default Training;
